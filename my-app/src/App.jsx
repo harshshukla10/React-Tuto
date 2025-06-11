9599293754d803b02707d95003a530041d01ca72
harsh <hs.jpg1034@gmail.com>
2025-06-11 10:01:16 +0530

    implemeting diffrent types of export/import

---
+++ my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Login from './usercomponent.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>
+          <h1>
+            Importing and exporting Component !!
+          </h1>
+          <Login></Login>
+       </div>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/usercomponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './usercomponent.css';
+
+function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+         Login user component  !!
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
